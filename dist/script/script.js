c4c5209e8a97ac2a28306bf4678504e67a43e3e8
wilsonelwilson <wvmc_es@hotmail.com>
2020-04-17 07:33:39 -0500

    Configuración y vinculación de carpetas pages

---
+++ dist/script/script.js
@@ -0,0 +1,3 @@
+"use strict";
+
+var arr = [1, 3, 4, 5];
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 
-let gulp = require('gulp');
-var browserSync = require("browser-sync").create();
-let sass = require('gulp-sass');
-let pug =  require('gulp-pug');
-let autoprefixer = require('gulp-autoprefixer');
-let imagemin = require('gulp-imagemin');
+const gulp = require('gulp');
+const browserSync = require("browser-sync").create();
+const sass = require('gulp-sass');
+const pug =  require('gulp-pug');
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+const babel = require('gulp-babel');
 
 gulp.task('sass', function () {
     return gulp.src('./dev/sass/*.scss')
@@ -28,15 +29,22 @@ exports.images = () => (
         .pipe(imagemin())
         .pipe(gulp.dest('dist/images'))
 );
+// Tarea babel
+exports.babel = () => (
+    gulp.src('./dev/script/*.js')
+    .pipe(babel({
+        presets: ['@babel/env']
+    }))
+    .pipe(gulp.dest('dist/script'))
 
-
+);
 
 //Tareas por defecto
 gulp.task('default',function(){
     gulp.watch('dev/**/*.pug', gulp.series('pug'))
     gulp.watch('dev/sass/**/*.scss', gulp.series('sass'))
     gulp.watch('dist/*.html').on('change', browserSync.reload)
-    gulp.watch('./dev/**/*.js').on('change', browserSync.reload)
+    gulp.watch('./dev/script/*.js').on('change', browserSync.reload)
     browserSync.init({
     server: {
     baseDir: "./dist/"
