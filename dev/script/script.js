0c786caeb556cacbb81c9e9aab5e7741977d3d8d
wilsonelwilson <wvmc_es@hotmail.com>
2020-04-19 14:46:19 -0500

    Animación progres bar con intersection observer

--- dev/script/script.js
+++ dev/script/script.js
@@ -4,4 +4,36 @@ let menu = document.querySelector('.header__hamburguer-menu').addEventListener('
     floatMenu.classList.toggle('active')
     barMenu.classList.toggle('toggle')
 })   
-    
+//Progress bar animations
+let progres = document.querySelectorAll('.about__progress-done')
+let valor = document.querySelectorAll('.about__progress .val')
+let scroll = document.querySelector('#scroll')
+
+let options = {
+    // root: null,
+    rootMargin: '0px',
+    threshold: 0
+
+    }
+
+function progressBar(entries){
+    if(entries[0].isIntersecting){
+        
+        for (const progre of progres) {
+        let dat =  progre.getAttribute('data-done')
+            progre.style.width = dat+'%';
+            progre.style.opacity = 1;
+
+        }
+        for (const val of valor) {
+            let score = val.textContent
+            val.style.left=score;
+            val.style.opacity=1;
+        }
+    }
+
+}
+
+
+let observer = new IntersectionObserver(progressBar, options);
+    observer.observe(scroll)
